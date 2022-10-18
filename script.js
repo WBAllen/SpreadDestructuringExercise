d62c9b6b90c976329bcf7dcb293d229b117f5255
Ben Allen <live.live.live.island@gmail.com>
2022-10-18 10:44:26 -0400

    destructuring

---
+++ script.js
@@ -0,0 +1,101 @@
+// 1a
+const mcuShows = [`Loki`, `Moon Knight`];
+console.log(mcuShows);
+
+// 1b
+const starWarsShows = [
+    `The Mandalorian`,
+    `The Book of Boba Fett`
+];
+console.log(starWarsShows);
+
+// 1c
+const disneyPlusShows = [
+    ...mcuShows,
+    ...starWarsShows,
+    `The Beatles: Get Back`
+];
+
+// 1d
+console.log(disneyPlusShows);
+
+// 2a
+const netflixMovies = {
+    action: `Extraction`,
+    crime: `The Irishman`
+};
+
+// 2b 
+const amazonPrimeMovies = {
+    action: `The Tomorrow War`,
+    drama: `One Night in Miami`
+};
+
+// 2c
+const streamingMovies = {
+    ...amazonPrimeMovies, 
+    ...netflixMovies,
+    musical: `Hamilton`
+};
+
+// 2d
+console.log (streamingMovies);
+
+// 3a
+const disneyJunior = [
+    `Mickey Mouse Clubhouse`,
+    `Spidey and his Amazing Friends`
+];
+
+// 3b
+const [mickey, spidey] = disneyJunior;
+
+// 3c
+console.log(mickey, spidey);
+
+// 4a
+const avengers = {
+    warMachine: `James Rhodes`,
+    theHulk: `Bruce Banner`,
+};
+
+// 4b
+const {warMachine, theHulk} = avengers;
+
+// 4c
+console.log(warMachine, theHulk);
+
+// 4d
+const moreAvengers = {
+    blackWidow: `Natasha Romanoff`,
+    hawkeye: `Clint Barton`,
+    ironMan: `Tony Stark`
+};
+
+// 4e
+const {blackWidow: nat, ...others} = moreAvengers;
+
+// 4f
+console.log(nat, others);
+
+const bonus = {
+    first: [1, 2, 3],
+    second: [4, 5, 6],
+    third: [7, 8, 9]
+  };
+
+// BONUS
+// 5a
+const all = [
+    ...bonus.first,
+    ...bonus.second,
+    ...bonus.third
+]
+
+console.log(all);
+
+// 5b
+const [one, two, three, four, five, six, seven, eight, nine] = all;
+
+console.log(one, two, three, four, five, six, seven, eight, nine);
+
